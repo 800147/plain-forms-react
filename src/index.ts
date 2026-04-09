113c9c800ad798a791c42b8f0452566bd0760e9b
Boolat Kamalov <800147@gmail.com>
2026-04-09 12:44:46 +0300

    export SubmitBlocker

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export * from "./components/ControlWrapper/ControlWrapper";
 export * from "./components/Form/Form";
+export * from "./components/SubmitBlocker/SubmitBlocker";
 export * from "./hooks/usePlainValidation";
 export * from "./types";
